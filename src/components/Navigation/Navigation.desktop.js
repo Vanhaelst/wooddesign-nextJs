7b1c22baad8ec4a26ce481f5856048ab8d6d0678
Indy Vanhaelst <indy.vanhaelst@hotmail.com>
2024-09-30 15:43:21 +0200

    Update topbar

--- src/components/Navigation/Navigation.desktop.js
+++ src/components/Navigation/Navigation.desktop.js
@@ -2,7 +2,7 @@ import React, { Fragment } from "react";
 import DesktopNavigation from "./default";
 
 const Navigation = () => {
-  return <DesktopNavigation id="nav" />
+  return <DesktopNavigation id="nav" />;
 };
 
 export default Navigation;
